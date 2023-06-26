a35051d9903efb3bcd25ef9b78d073acc8b15ee8
Renata Hassum <renatahassum@gmail.com>
2023-06-26 17:55:34 -0300

    User get user posts

--- backend/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ backend/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.workshopmongo.services;
 
+import com.devsuperior.workshopmongo.models.dto.PostDTO;
 import com.devsuperior.workshopmongo.models.dto.UserDTO;
 import com.devsuperior.workshopmongo.models.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
@@ -54,6 +55,11 @@ public class UserService {
     public void delete(String id) {
         getEntityById(id);
         repository.deleteById(id);
+    }
+
+    public List<PostDTO> getUserPosts(String id) {
+        User user = getEntityById(id);
+        return user.getPosts().stream().map(PostDTO::new).collect(Collectors.toList());
     }
 
 }
