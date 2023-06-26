affbc9772e6dd1e863e8ff6cf60959ec2e06940e
Renata Hassum <renatahassum@gmail.com>
2023-06-26 17:41:19 -0300

    User insert

--- backend/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ backend/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -17,18 +17,33 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<UserDTO> findAll() {
-        List<User> list = repository.findAll();
-        return list.stream().map(UserDTO::new).collect(Collectors.toList());
-    }
-
     private User getEntityById(String id) {
         Optional<User> result = repository.findById(id);
         return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+    }
+
+    private void copyDtoToEntity(UserDTO dto, User entity) {
+        entity.setName(dto.getName());
+        entity.setEmail(dto.getEmail());
+    }
+
+    public List<UserDTO> findAll() {
+        List<User> list = repository.findAll();
+        return list.stream().map(UserDTO::new).collect(Collectors.toList());
     }
 
     public UserDTO findById(String id) {
         User entity = getEntityById(id);
         return new UserDTO(entity);
     }
+
+    public UserDTO insert(UserDTO dto) {
+        User entity = new User();
+        copyDtoToEntity(dto, entity);
+        entity = repository.insert(entity);
+        return new UserDTO(entity);
+    }
+
+
+
 }
