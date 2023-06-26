41a3f33707104e2970f038ead650a9daebd8ca53
Renata Hassum <renatahassum@gmail.com>
2023-06-26 17:03:51 -0300

    References Author, Comment, Post

--- backend/src/main/java/com/devsuperior/workshopmongo/config/TestConfig.java
+++ backend/src/main/java/com/devsuperior/workshopmongo/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.devsuperior.workshopmongo.config;
 
 import com.devsuperior.workshopmongo.models.entities.User;
+import com.devsuperior.workshopmongo.models.repositories.PostRepository;
 import com.devsuperior.workshopmongo.models.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +16,13 @@ public class TestConfig {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @PostConstruct
     public void init() {
         userRepository.deleteAll(); //zerar banco
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
